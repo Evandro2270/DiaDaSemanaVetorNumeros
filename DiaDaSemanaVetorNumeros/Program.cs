279123aff450cb9e4ebf7fa8fc6c7c76a333a33e
Evandro2270 <evandro535@gmail.com>
2024-12-14 17:16:37 -0300

    Adicionar arquivos de projeto.

---
+++ DiaDaSemanaVetorNumeros/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                int[,] numeros = new int[5,3];
+
+                numeros = new int[,] { { 3, 13 }, { 4, 6 }, { 8, 2 }, { 9, 1 }, { 10, 2 }, };
+
+                Console.WriteLine();
+                foreach (int resultado in numeros)
+                {
+                    Console.WriteLine(resultado);
+                }
+
+            }
+        }
+    }
+}
